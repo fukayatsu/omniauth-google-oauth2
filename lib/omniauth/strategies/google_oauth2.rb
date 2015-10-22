c095eaa124a6dba44c241e118d7e887761e1eae0
fukayatsu <fukayatsu@gmail.com>
2015-10-22 16:49:50 +0900

    Override callback_url

--- lib/omniauth/strategies/google_oauth2.rb
+++ lib/omniauth/strategies/google_oauth2.rb
@@ -22,6 +22,11 @@ module OmniAuth
         :token_url     => '/o/oauth2/token'
       }
 
+      # HACK: https://github.com/intridea/omniauth-oauth2/pull/70
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       def authorize_params
         super.tap do |params|
           options[:authorize_options].each do |k|
